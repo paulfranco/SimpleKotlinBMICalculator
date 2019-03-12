e24fbb3ef675f706ed41421016a6f65f53256754
Paul Franco <paulfranco@me.com>
2019-03-12 02:10:20 -0700

    Added resources

--- app/src/main/java/co/paulfran/paulfranco/simplekotlinbmicalculator/MainActivity.kt
+++ app/src/main/java/co/paulfran/paulfranco/simplekotlinbmicalculator/MainActivity.kt
@@ -2,11 +2,15 @@ package co.paulfran.paulfranco.simplekotlinbmicalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun calculate(view: View) {
+
     }
 }
